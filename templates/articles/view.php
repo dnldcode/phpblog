2d035f29c415a2f35dbc16a55b37179ce576338c
Serhii Holishevskyi <dnldcode@gmail.com>
2019-03-23 12:45:34 +0100

    Article's author clickable

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -1,7 +1,7 @@
 <?php include __DIR__ . '/../header.php'; ?>
     <h2><?= $article->getName() ?></h2>
     <p><?= $article->getText() ?></p>
-    <p>Автор: <?= $article->getAuthor()->getNickname() ?></p>
+    <p>Автор: <a href="/user/id<?= $article->getAuthorId() ?>"><?= $article->getAuthor()->getNickname() ?></a></p>
     <?php if ($user !== null && $user->isAdmin()): ?>
     <a href="/articles/<?= $article->getId() ?>/edit" class="btn btn-primary" style="color: white">Редактировать статью</a>
     <?php endif; ?>
